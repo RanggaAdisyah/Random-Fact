fe87776af08f4905428a3840d1fbd609d31aab34
RanggaAdisyah <116618151+RanggaAdisyah@users.noreply.github.com>
2024-12-10 15:34:29 +0700

    Enhance fact display and cookie management in Random Fact Generator

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function fetchFact() {
         .then(data => {
             const factText = data.text ? data.text : 'Sorry, no fact found!';
             
-            document.getElementById('fact').innerHTML = `<strong>Fact (English):</strong> ${factText}`;
+            document.getElementById('fact').innerHTML = `<p><strong>Fact (English):</strong> ${factText}<p>`;
 
             translateToIndonesian(factText);
         })
@@ -22,10 +22,52 @@ function translateToIndonesian(text) {
         .then(data => {
             const translatedText = data.responseData.translatedText;
             
-            document.getElementById('fact').innerHTML += `<br><strong>Fact (Indonesia):</strong> ${translatedText}`;
+            document.getElementById('fact').innerHTML += `<p><strong>Fact (Indonesia):</strong> ${translatedText}<p>`;
+
+            setCookie('factEng', text, 1);
+            setCookie('factId', translatedText, 1); 
+
         })
         .catch(error => {
             console.error('Error translating fact:', error);
             document.getElementById('fact').innerHTML += '<br><strong>Failed to translate.</strong>';
         });
 }
+
+function displayFact(language, text) {
+    const factContainer = document.getElementById('fact');
+    const factHtml = `<p><strong>Fact (${language}):</strong> ${text}</p>`;
+    factContainer.innerHTML += factHtml;
+}
+
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + encodeURIComponent(value) + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        if (cookie.indexOf(name + "=") === 0) {
+            return decodeURIComponent(cookie.substring(name.length + 1));
+        }
+    }
+    return "";
+}
+
+function loadLastFact() {
+    const factEng = getCookie('factEng');
+    const factId = getCookie('factId');
+
+    if (factEng && factId) {
+        displayFact('English', factEng);
+        displayFact('Indonesia', factId);
+    } else {
+        document.getElementById('fact').innerHTML = '<p>No fact found. Click the button to get a new one!</p>';
+    }
+}
+
+window.onload = loadLastFact;
